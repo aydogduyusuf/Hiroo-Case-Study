d12e6ed9f081b4d8e2cbf16a2bea5ae25ee166de
aydogduyusuf <yusufaydogdu23@gmail.com>
2024-04-07 02:18:16 +0300

    spring initialize

---
+++ server/src/main/java/com/Hiroo/talentSourcing/TalentSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.Hiroo.talentSourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentSourcingApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/Hiroo/talentSourcing/TalentSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hiroo.talentSourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
